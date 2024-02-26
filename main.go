23a02dfca04f8b497960dcf75eb570985253fa5f
notnuff <113949291+notnuff@users.noreply.github.com>
2024-02-26 15:19:50 +0200

    our first rebase UwU

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"lab1-GOys/server"
 )
 
 func main() {
-	server.PrintHello()
 	fmt.Println("GOing crazy")
 }
--- server/server.go
+++ server/server.go
@@ -13,3 +13,8 @@ func GetTime() {
 	currentTime := time.Now()
 	fmt.Println("time:", currentTime)
 }
+
+const (
+	PORT  string = ":8795"
+	ROUTE string = "/time"
+)
